ae219e7422e815e01312181a2be4e8581d63f84a
Elisabeth <elise.doisneau@gmail.com>
2023-11-29 19:03:40 +0100

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,15 +9,21 @@ import eliphoto from "./../../assets/images/eliphoto.png";
 const About = () => {
   return (
     <section id="about">
-      <h2>About Me</h2>
-
       <div className="container about__container">
         <div className="about__me-image">
           <img src={eliphoto} className="eliphoto" alt="About"></img>
         </div>
+        <div className="about__me-heading"> 
+        <div className="h1"> 
+        Hi👋 I'm Elisabeth a Frontend Developer based in Germany.
+        </div>
+        </div>
+        </div>
 
-        <div className="about__me-text">
-          <p>
+
+        <div className="about__me-text"> 
+        <div className="h2">About Me</div>
+          <p className="text">
             I am a full stack web developer with a passion for creating
             beautiful, responsive, and functional websites. I have a background
             in graphic design and a passion for learning new technologies. I am
@@ -29,14 +35,14 @@ const About = () => {
             for new opportunities to work with a team of developers.{" "}
           </p>
         </div>
-      </div>
+     
 
       <div className="about__content">
         <div className="about__cards">
           <article className="about__card">
             <FaAward className="about__icon" />
             <h5> Experience </h5>
-            <small> 3+ Years working </small>
+            <small> 2+ Years working </small>
           </article>
 
           <article className="about__card">

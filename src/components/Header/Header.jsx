3133204be3c050a6ef5e5133413c2dd997b78424
Elisabeth <elise.doisneau@gmail.com>
2023-11-29 19:55:53 +0100

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+import eliphoto from "../../assets/images/eliphoto.png";
+
+const Header = () => {
+  return (
+    <div className="header__container">
+      <div className="image__container">
+        <img src={eliphoto} className="eliphoto" alt="About"></img>
+      </div>
+
+      <div className="header__text">
+        <header className="header">
+          <h1 className="header__heading">
+            Hi👋 I'm Elisabeth <br/>
+            a <span> Frontend Developer</span> <br/> based in <span>Germany</span> . 
+            </h1> 
+ 
+        </header>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

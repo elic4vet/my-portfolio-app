4575a51167aaeb68c04c9252c878c547f54abfa9
Elisabeth <elise.doisneau@gmail.com>
2023-11-29 18:38:05 +0100

    +snow
    added snow to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Snowfall from "react-snowfall";
 import About from "./components/About/About";
 import "./App.css";
 
@@ -10,6 +11,13 @@ function App() {
       <About />
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+      <Snowfall
+  style={{
+    position: 'fixed',
+    width: '100vw',
+    height: '100vh',
+  }}
+/>
     </>
   );
 }

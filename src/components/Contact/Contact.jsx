7a046f56c53594fa3e94052bf77211591b116d5f
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-12-05 22:27:18 +0100

    +contact section

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact_container" id="contact">
+      <h1>Contact</h1>
+      <p>Phone: 555-555-5555</p>
+      <p>
+        Email:
+        <a href="mailto:" target="_blank" rel="noopener noreferrer" />
+      </p>
+    </div>
+  );
+};
+
+export default Contact;

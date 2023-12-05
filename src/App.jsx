94803c6d2f182950b14764a17b1756d47ef173e1
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-12-05 22:27:23 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Snowfall from "react-snowfall";
 import About from "./components/About/About";
 import Header from "./components/Header/Header";
+import Contact from "./components/Contact/Contact";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <>
       <Header />
       <About />
+      <Contact />
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <Snowfall

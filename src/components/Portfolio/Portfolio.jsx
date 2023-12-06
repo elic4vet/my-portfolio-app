c5f15a010d1c76b20c8f44b6e30bd69354072a21
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-12-06 23:04:02 +0100

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,24 +1,39 @@
 import React from "react";
+import "./Portfolio.css";
 
 const Portfolio = () => {
   return (
     <div>
       <h1>Portfolio</h1>
       <p>Here are some of my projects:</p>
-      <ul>
-        <li>
-          <a href="2">Project 1</a>
-        </li>
-        <li>
-          <a href="3">Project 2</a>
-        </li>
-        <li>
-          <a href="4">Project 3</a>
-        </li>
-        <li>
-          <a href="5">Project 4</a>
-        </li>
-      </ul>
+
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card card1">
+            <img src="..." className="card-img-top" alt="..." />
+            <h5 className="card-title">Project 1</h5>
+            <p className="card-text">Description</p>
+            <a href="#" className="btn btn-primary">
+              Live preview
+            </a>
+            <a href="#" className="btn btn-primary">
+              View Code
+            </a>
+          </div>
+        </div>
+
+        <div className="card card2">
+          <img src="..." className="card-img-top" alt="..." />
+          <h5 className="card-title">Project 2</h5>
+          <p className="card-text">Description</p>
+          <a href="#" className="btn btn-primary">
+            Live preview
+          </a>
+          <a href="#" className="btn btn-primary">
+            View Code
+          </a>
+        </div>
+      </div>
     </div>
   );
 };

c44e7542e6b77cb971825380382d4afda69c64d0
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-12-06 21:49:10 +0100

    +portfolio section

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <p>Here are some of my projects:</p>
+      <ul>
+        <li>
+          <a href="2">Project 1</a>
+        </li>
+        <li>
+          <a href="3">Project 2</a>
+        </li>
+        <li>
+          <a href="4">Project 3</a>
+        </li>
+        <li>
+          <a href="5">Project 4</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Portfolio;

18ad3b98d8b8fc972f3062c77b3400193dbbf069
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-12-10 21:43:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Header from "./components/Header/Header";
 import Contact from "./components/Contact/Contact";
 import "./App.css";
 import Portfolio from "./components/Portfolio/Portfolio";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <>
+      <Sidebar />
       <Header />
       <About />
       <Portfolio />

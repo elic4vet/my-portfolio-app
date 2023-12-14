70df1551616afc8ab3db87a15680198950b8b8cb
Elisabeth <elise.doisneau@gmail.com>
2023-12-14 19:41:34 +0100

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,15 +5,15 @@ const Contact = () => {
   return (
     <div className="contact_container" id="contact">
       <h1>Contact</h1>
-      <p>Phone: 555-555-5555</p>
+ 
       <p>
         Email:{" "}
         <a
           href="mailto:elisabeth.erkekoglou@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
-        />
-        elisabeth.erkekoglou@gmail.com
+        >
+ <i class="fa fa-envelope"></i> </a>
       </p>
     </div>
   );

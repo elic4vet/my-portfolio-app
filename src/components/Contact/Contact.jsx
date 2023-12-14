a85496e6dc8f7ea9d9a1cd7b100875bf9986edb6
Elisabeth <elise.doisneau@gmail.com>
2023-12-14 20:17:40 +0100

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,6 +15,34 @@ const Contact = () => {
         >
  <i class="fa fa-envelope"></i> </a>
       </p>
+
+ <div className="socials">
+      <a
+          href="https://www.linkedin.com/in/elisabeth-erkekoglou/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a
+        href="https://github.com/elic4vet">
+          <i class="fab fa-github"></i>
+        </a>
+        <a
+          href="https://www.instagram.com/elisa.codes23/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-facebook"></i>
+        </a>
+        </div>
     </div>
   );
 };
